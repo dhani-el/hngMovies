02b69f3882cb4f90af4f9ff5ce5b0263a9138c61
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2023-09-16 21:41:18 +0100

    implment fetch for single movie page

--- src/SingleMovie/index.jsx
+++ src/SingleMovie/index.jsx
@@ -2,19 +2,21 @@ import { LeftSection, MainSection } from "./jsx/component"
 import { useState,useEffect } from "react";
 import axios from "axios";
 import "./styles/index.css"
+import {useParams} from "react-router-dom"
 
 
 
 export default function SingleMovie(){
-    const [movieData, setMovieData] = useState([])
-    const id = 1002185
+
+    const [movieData, setMovieData] = useState(null)
+    const {id} = useParams()
    useEffect(function(){
         const data = axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=61cd0c8cecd52ead927518a62ef33472&append_to_response=recommendations`)
         .then(info=>{console.log(info.data); setMovieData(info.data)});
     },[]);
     return <div id="singelMovieDiv">
                 <LeftSection/>
-                <MainSection/>
+                {movieData == null ?<div>...loading</div>:<MainSection data={movieData} />}
             </div>
 }
 
--- src/SingleMovie/jsx/component.jsx
+++ src/SingleMovie/jsx/component.jsx
@@ -3,7 +3,6 @@ import { HomeOutlined,MovieOutlined,TvOutlined,CalendarMonthOutlined, Logout,Sta
 import { Button } from "@mui/material";
 import "../styles/components.css";
 import tv from "../../assets/image/tv.png";
-import poster from "../../assets/image/testimage.jpg"
 
 
 export function LeftSection(){
@@ -66,7 +65,7 @@ function LeftSectionLogOut(){
 }
 
 
-export function MainSection(){
+export function MainSection({data}){
     const dummyData = {
         title :"Top Gun: Maverick",
         year :"2022",
@@ -83,47 +82,44 @@ export function MainSection(){
         recomendedImage:["","",""]
     }
     return <div id="mainSection">
-                <MainSectionVideo videoSrc={""}/>
+                <MainSectionVideo videoSrc={data.video} poster={`http://image.tmdb.org/t/p/original/${data.backdrop_path}`}/>
                 <div id="metaDataContainer">
-                    <MainSectionMetaData someMetadata={dummyData}/>
-                    <MainSectionExtraData extraData={{extraRating:dummyData.extraRating,reviewers:dummyData.reviewers,recomendedImage:dummyData.recomendedImage}}/>
+                    <MainSectionMetaData someMetadata={data}/>
+                    <MainSectionExtraData extraData={{vote_average:data.vote_average,vote_count:data.vote_count,recomendedImage:dummyData.recomendedImage}}/>
                 </div>
             </div>
 }
 
-function MainSectionVideo({videoSrc}){
-    return <div id="MainSectionVideo"><video controls  poster={poster}> <source src=""  type="video/mp4"/> </video></div>
+function MainSectionVideo({videoSrc,poster}){
+    return <div id="MainSectionVideo"><video controls  poster={poster}> <source src={videoSrc == false ? false:videoSrc}  type="video/mp4"/> </video></div>
 }
 
 function MainSectionMetaData({someMetadata}){
-    const data  = fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=61cd0c8cecd52ead927518a62ef33472",{method:"GET"})
-    .then(function(result){
-    console.log(result);
-        return result});
+    console.log(someMetadata);
     return <div id="MainSectionMetaData">
                 <div id="basicData">
                     <p id="title" data-testid = "movie-title">{someMetadata.title}</p>
-                    <p id="year" className="dot" data-testid = "movie-release-date">{new Date(someMetadata.year).toUTCString()}</p>
-                    <p id="rating" className="dot">{someMetadata.rating}</p>
-                    <p id="duration" className="dot" data-testid = "movie-runtime" >{someMetadata.duration}</p>
-                    <div id="listOfGenre">{someMetadata.genre.map(function(aGenre){
-                           return <span key={aGenre} id="Agenre">{`${aGenre} `}</span>
+                    <p id="year" className="dot" data-testid = "movie-release-date">{someMetadata.release_date}</p>
+                    <p id="rating" className="dot">PG-13</p>
+                    <p id="duration" className="dot" data-testid = "movie-runtime" >{someMetadata.runtime} mins</p>
+                    <div id="listOfGenre">{someMetadata.genres.map(function(aGenre){
+                           return <span key={aGenre} id="Agenre">{`${aGenre.name} `}</span>
                     })}</div>
                 </div>
-                <p id="synopsis" data-testid = "movie-overview">{someMetadata.synopsis}</p>
+                <p id="synopsis" data-testid = "movie-overview">{someMetadata.overview}</p>
                 <div id="cast" >
                     <div>
-                        <p id="directors">Director : </p><p className="value"> {someMetadata.director}</p>
+                        <p id="directors">Director : </p><p className="value"> {someMetadata.director?someMetadata.director:"N/A"}</p>
                     </div>
                     <div>
-                        <p id="writers">Writers : </p><p className="value"> {Array.from(someMetadata.writers).join(", ")}</p>
+                        <p id="writers">Writers : </p><p className="value"> {someMetadata.writers ? Array.from(someMetadata.writers).join(", "):"N/A"}</p>
                     </div>
                     <div>
-                        <p id="stars">Stars : </p><p className="value"> {Array.from(someMetadata.stars).join(", ")}</p>
+                        <p id="stars">Stars : </p><p className="value"> {someMetadata.stars ? Array.from(someMetadata.stars).join(", "):"N/A"}</p>
                     </div>
                 </div>
                 <div id="acheivement">
-                    <span>Top rated movie {`#${someMetadata.topRating}`}</span>
+                    <span>Top rated movie {`#${Math.floor(someMetadata.popularity)}`}</span>
                     <select>
                         <option>Awards 9 nominations</option>
 
@@ -137,8 +133,8 @@ export function MainSectionExtraData({extraData}){
                 <div id="extraRating">
                     <Star/>
                     <span id ="ratingSpan">
-                        <p id="greyText">{extraData.extraRating} </p>
-                        <p> | {extraData.reviewers}</p>
+                        <p id="greyText">{extraData.vote_average} </p>
+                        <p> | {extraData.vote_count}</p>
                     </span>
                 </div>
                 <div id="callToActions">
